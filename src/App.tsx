ed5421d9b57cef818c5f6560481829f90300306a
jhpark <qkr13wlgus57@gmail.com>
2021-06-11 18:48:35 +0900

    candy List up

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { useRef } from 'react';
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 
-import { Candy, Video, ShowFace } from './components';
-import TextFace from './components/TestFace';
+import { Candy, Video, ShowFace } from "./components";
+import TextFace from "./components/TestFace";
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  // const videoRef = useRef<HTMLVideoElement>(null);
+  const [candyList, setCandyList] = useState<any>([]);
+  let max = 3000;
+
+  useEffect(() => {
+    makeCandy();
+  }, []);
+
+  const makeCandy = () => {
+    if (max <= 0) return false;
+
+    setCandyList(candyList.push(<Candy />));
+    max -= 10;
+    setTimeout(() => {
+      makeCandy();
+    }, max);
+  };
 
   return (
     <div className="App">
       {/* <Video videoRef={videoRef} /> */}
       <TextFace />
-      <Candy />
-      {/* <ShowFace videoRef={videoRef} /> */}
+      {candyList.length > 0 &&
+        candyList.map((NewCandy: any, key: number) => <NewCandy key={key} />)}
+      ;{/* <ShowFace videoRef={videoRef} /> */}
     </div>
   );
 }
--- src/components/ShowFace.tsx
+++ src/components/ShowFace.tsx
@@ -1,9 +1,9 @@
-import { useFaceApi } from '../hooks/useFaceApi';
-import { useEffect } from 'react';
-import React from 'react';
+import { useFaceApi } from "../hooks/useFaceApi";
+import { useEffect } from "react";
+import React from "react";
 
 const myFaceApiConfig = {
-  input: 'my-image',
+  input: "my-image",
   refreshRate: 250,
 };
 const ShowFace = ({ videoRef }: { videoRef: any }) => {
@@ -13,24 +13,25 @@ const ShowFace = ({ videoRef }: { videoRef: any }) => {
     <>
       {faces &&
         videoRef &&
-        'current' in videoRef &&
+        "current" in videoRef &&
         faces.map((face) => {
           // Attributes
           const top = face.relativeBox.top * videoRef.current.offsetHeight;
           const left = face.relativeBox.left * videoRef.current.offsetWidth;
           const width = face.relativeBox.width * videoRef.current.offsetWidth;
-          const height = face.relativeBox.height * videoRef.current.offsetHeight;
+          const height =
+            face.relativeBox.height * videoRef.current.offsetHeight;
 
           // Rendering
           return (
             <div
               style={{
-                position: 'absolute',
+                position: "absolute",
                 width: width,
                 height: height,
                 left: left,
                 top: top,
-                border: '1px solid red',
+                border: "1px solid red",
               }}
             />
           );
