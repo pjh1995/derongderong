a7f664936cf3819ad5148c47cc1e262b518b9f63
jhpark <qkr13wlgus57@gmail.com>
2021-06-14 18:53:56 +0900

    Add recoil

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil'; // recoil 추가
 import './index.css';
 import App from './App';
 
+const app = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root'),
+  app,
 );
